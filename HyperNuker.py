bb075d300539e6764613ba232dd5934ad6a5b736
Snu223 <snuzin.top@gmail.com>
2022-02-19 18:13:49 -0300

    main commit

--- HyperNuker.py
+++ HyperNuker.py
@@ -1,6 +1,30 @@
 # By Snu
 # The creator is not responsible for misuse
 
+"""
+MIT License
+
+Copyright (c) 2022 Snu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from discord.ext import commands
 from files.commands import raid, extra
 from requests import get
@@ -38,8 +62,6 @@ with open("files/config.json", "r") as file:
     elif get("https://discordapp.com/api/v9/users/@me", headers={"Authorization": TOKEN}).status != 200:
         print(f"{Fore.RED}[ERRO] token inválida{Style.RESET_ALL}")
         exit(0)
-    
-    
 
 client = commands.Bot(
     command_prefix=PREFIXO,
